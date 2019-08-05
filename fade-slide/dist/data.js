1c6446b623af9aa07e6521b1c3d2b09cec5838dc
GX18 <phambichan0505@gmail.com>
2019-08-05 18:11:45 +0700

    d

--- fade-slide/dist/data.js
+++ fade-slide/dist/data.js
@@ -13,8 +13,8 @@ data = [
         a1: "The route towards the castle",
         a2: "The route towards the sea",
         a3: "The route towards the jungle",
-        a4: "",
-        imgSrc: ""
+        a4: "The route towards the sky",
+        imgSrc: "https://images.unsplash.com/photo-1562948853-963f5ab6a434?ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80",
     },
 
     {
@@ -22,35 +22,35 @@ data = [
         a1: "A white dagger that has a red gem carved in the middle of the handle",
         a2: "A book full of enchantment and magic",
         a3: "A golden key",
-        a4: "",
-        imgSrc: ""
+        a4: "A feather that can grant your wish",
+        imgSrc: "https://images.unsplash.com/photo-1553991562-9f24b119ff51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1274&q=80",
     },
 
     {
-        question: "Before making a decision, on which do you rely?",
-        a1: "Your head",
-        a2: "Your heart",
-        a3: "Your friend/both",
-        a4: "",
-        imgSrc: ""
+        question: "Who are you?",
+        a1: "A pirate on its journey to the neverland",
+        a2: "A princess in its locked tower",
+        a3: "A witch with a wicked sin",
+        a4: "A knight in shining armour",
+        imgSrc: "https://images.unsplash.com/photo-1521616210349-dbec8efd13b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     },
 
     {
-        question: "You prefer which players of cards?",
+        question: "Your chosen cards?",
         a1: "The Ace",
-        a2: "YThe Queen",
+        a2: "The Queen",
         a3: "The King",
         a4: "The Jack",
-        imgSrc: ""
+        imgSrc: "https://images.unsplash.com/photo-1532152734721-b08cb57943aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     },
 
     {
-        question: "What is your sign?",
+        question: "Sign of soul?",
         a1: "The Heart",
         a2: "The Diamond",
         a3: "The Spade",
         a4: "The Clover",
-        imgSrc: ""
+        imgSrc: "https://images.unsplash.com/photo-1542598688-5597ff6d3cf4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80",
     },
 
     {
@@ -58,8 +58,8 @@ data = [
         a1: "Try to negotiate a peacful method",
         a2: "Bring on a 1-1 battle",
         a3: "Just ignore them",
-        a4: "Call a friend for help",
-        imgSrc: ""
+        a4: "Call for help",
+        imgSrc: "https://images.unsplash.com/photo-1560090628-5fa60a573240?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1474&q=80",
     },
     {
         question: "What is your favourite cake?",
@@ -67,15 +67,15 @@ data = [
         a2: "Donut",
         a3: "Black forest cake",
         a4: "Moon cake",
-        imgSrc: ""
+        imgSrc: "https://images.unsplash.com/photo-1426869884541-df7117556757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     },
     {
         question: "Which is your favourtie animal?",
-        a1: "Tree",
-        a2: "Human",
+        a1: "Fox",
+        a2: "Cat",
         a3: "Spider",
         a4: "Dog",
-        imgSrc: ""
+        imgSrc: "https://images.unsplash.com/photo-1562821090-797af8a39e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     },
     {
         question: "What is your dream job among this:",
@@ -83,7 +83,7 @@ data = [
         a2: "Design",
         a3: "IT",
         a4: "Cakery",
-        imgSrc: ""
+        imgSrc: "https://images.unsplash.com/photo-1462206092226-f46025ffe607?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1506&q=80",
     },
     {
         question: "Who are you?",
@@ -91,7 +91,7 @@ data = [
         a2: "The presenter ",
         a3: "The rebel",
         a4: "The silent sacrifice",
-        imgSrc: ""
+        imgSrc: "https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     },
     {
         question: "Choose a card?",
@@ -99,15 +99,15 @@ data = [
         a2: "The Empress",
         a3: "Knight of Swords",
         a4: "The hanged man",
-        imgSrc: ""
+        imgSrc: "https://images.unsplash.com/photo-1562532755-1c8e8e323201?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     },
     {
-        question: "Your dream field?",
-        a1: "A firefighter",
-        a2: "A vlogger",
-        a3: "Baseball player/ Athletic",
-        a4: "Teacher",
-        imgSrc: ""
+        question: "What is your house?",
+        a1: "Vulpes",
+        a2: "Cerberus",
+        a3: "Succubus",
+        a4: "Griffin",
+        imgSrc: "https://images.unsplash.com/photo-1504346879257-9d899883589f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     },
     {
         question: "You accidentally erased some of the magic potion in the lab, and you can only recreate one, which one would you choose?",
@@ -115,7 +115,7 @@ data = [
         a2: "The potion that can grant you eternal beauty and youth",
         a3: "The potion that can grant you power",
         a4: "The potion that give you wisdom",
-        imgSrc: ""
+        imgSrc: "https://images.unsplash.com/photo-1546201483-8886694038aa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
     },
 
 ]
